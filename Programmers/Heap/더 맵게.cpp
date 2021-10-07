0004f7cfa5346aa175424f99e91c1c26ee58f77b
jhy979 <32920566+jhy979@users.noreply.github.com>
2021-10-07 20:17:11 +0900

    Create 더 맵게.cpp

---
+++ Programmers/Heap/더 맵게.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <iostream>
+using namespace std;
+
+int solution(vector<int> scoville, int K) {
+    int answer = 0;
+    priority_queue <int,vector<int>,greater<int>> pq;
+    
+    for(int i = 0 ; i <scoville.size();i++){
+        pq.push(scoville[i]);
+    }
+    
+    while(pq.size()>1){
+        int first = pq.top(); pq.pop();
+        int second = pq.top(); pq.pop();
+        pq.push(first+second*2);
+        answer++;
+        if(pq.top()>=K){
+            return answer;
+        }
+    }
+    
+    return -1;
+}
